7e093840aeb5bb32b58f17f5af22747b69e75e4e
megmog88 <meghanspence88@hotmail.co.uk>
2021-02-09 15:30:49 +0000

    added wineinfo.html and changed css style, started adding navbar to python

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
+from flask_nav import Nav
+from flask_nav.elements import Navbar, View
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
+
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
@@ -22,6 +25,18 @@ mongo = PyMongo(app)
 def get_winelist():
     wine_list = mongo.db.wine_list.find()
     return render_template("wines.html", wine_list=wine_list)
+
+# Building NavBar with Flask
+nav = Nav(app)
+
+nav.register_element("navbar", Navbar(
+    "thenav",
+    View("Home Page", "index")))
+
+
+@app.route("/")
+def index():
+    return render_template("base.html")
 
 
 if __name__ == "__main__":
